a624708291cdd657520c8d8fba38fabfa483dd86
Ricardo Rodriguez Martin <37267193+Riroma13@users.noreply.github.com>
2018-03-11 23:01:48 +0100

    Añadido prueba2

--- GitHub/src/github/ClasePrueba.java
+++ GitHub/src/github/ClasePrueba.java
@@ -2,7 +2,9 @@ package github;
 
 public class ClasePrueba {
 	private String prueba;
+	private String prueba2;
 	public ClasePrueba(){
-		prueba="prueba";		
+		prueba="prueba";
+		prueba2="prueba2";
 	}
 }
