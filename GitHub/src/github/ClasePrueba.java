cf2fa6b4e52c20ae7d81af93cf65493dc5ebf451
Rovaro <alvarorm94@gmail.com>
2018-03-11 23:36:29 +0100

    Cambio añadido por un colaborador

--- GitHub/src/github/ClasePrueba.java
+++ GitHub/src/github/ClasePrueba.java
@@ -3,6 +3,8 @@ package github;
 public class ClasePrueba {
 	private String prueba;
 	private String prueba2;
+	
+	// Éste es el constructor de la clase (escrito por Rovaro)
 	public ClasePrueba(){
 		prueba="prueba";
 		prueba2="prueba2";
